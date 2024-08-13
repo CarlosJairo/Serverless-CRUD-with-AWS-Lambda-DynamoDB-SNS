75978824cf810954cbb688b07d6c140d75b2c590
CarlosJairo <carlosjairo38@gmail.com>
2024-08-13 14:15:14 -0500

    HU8-completando autenticacion en el back

---
+++ src/getToken.js
@@ -0,0 +1,56 @@
+const AWS = require("aws-sdk");
+const cognito = new AWS.CognitoIdentityServiceProvider();
+
+exports.getToken = async (event) => {
+  const { username, password } = JSON.parse(event.body);
+
+  const params = {
+    AuthFlow: "USER_PASSWORD_AUTH",
+    ClientId: "625bq36ubnoros3mpfabp9tn79",
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password,
+    },
+  };
+
+  try {
+    // Primer intento de autenticación
+    let response = await cognito.initiateAuth(params).promise();
+
+    if (response.ChallengeName === "NEW_PASSWORD_REQUIRED") {
+      const newPassword = password;
+
+      const newPasswordParams = {
+        ClientId: "625bq36ubnoros3mpfabp9tn79",
+        ChallengeName: "NEW_PASSWORD_REQUIRED",
+        Session: response.Session,
+        ChallengeResponses: {
+          NEW_PASSWORD: newPassword,
+          USERNAME: username,
+        },
+      };
+
+      // Establece la nueva contraseña
+      response = await cognito
+        .respondToAuthChallenge(newPasswordParams)
+        .promise();
+    }
+
+    // Devuelve el token
+    console.log("Token JWT:", response.AuthenticationResult.IdToken);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        token: response.AuthenticationResult.IdToken,
+      }),
+    };
+  } catch (error) {
+    console.error("Error al obtener el token:", error.message);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
+};
--- src/hello.js
+++ src/hello.js
@@ -6,3 +6,62 @@ exports.hello = async (event) => {
     }),
   };
 };
+
+// const AWS = require("aws-sdk");
+// const cognito = new AWS.CognitoIdentityServiceProvider();
+
+// exports.hello = async (event) => {
+//   const username = "carlosjairo38@gmail.com"; // Coloca aquí tu nombre de usuario
+//   const password = "12345678"; // Coloca aquí tu contraseña
+
+//   const params = {
+//     AuthFlow: "USER_PASSWORD_AUTH",
+//     ClientId: "625bq36ubnoros3mpfabp9tn79", // Reemplaza con tu Client ID de Cognito
+//     AuthParameters: {
+//       USERNAME: username,
+//       PASSWORD: password,
+//     },
+//   };
+
+//   try {
+//     // Primer intento de autenticación
+//     let response = await cognito.initiateAuth(params).promise();
+
+//     // Si el flujo requiere una nueva contraseña
+//     if (response.ChallengeName === "NEW_PASSWORD_REQUIRED") {
+//       const newPassword = "NuevaContraseña"; // Establece aquí la nueva contraseña
+
+//       const newPasswordParams = {
+//         ClientId: "625bq36ubnoros3mpfabp9tn79", // Reemplaza con tu Client ID de Cognito
+//         ChallengeName: "NEW_PASSWORD_REQUIRED",
+//         Session: response.Session,
+//         ChallengeResponses: {
+//           NEW_PASSWORD: newPassword,
+//           USERNAME: "carlosjairo38@gmail.com", // Asegúrate de incluir el nombre de usuario aquí
+//         },
+//       };
+
+//       // Establece la nueva contraseña
+//       response = await cognito
+//         .respondToAuthChallenge(newPasswordParams)
+//         .promise();
+//     }
+
+//     // Devuelve el token
+//     console.log("Token JWT:", response.AuthenticationResult.IdToken); // Imprime el token en la consola
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         token: response.AuthenticationResult.IdToken,
+//       }),
+//     };
+//   } catch (error) {
+//     console.error("Error al obtener el token:", error.message);
+//     return {
+//       statusCode: 400,
+//       body: JSON.stringify({
+//         error: error.message,
+//       }),
+//     };
+//   }
+// };
